0b4a97b7e8882841d80226625a31bbd643b3d97d
nikolaj schlüter.nielsen <nikolaj.sn@hotmail.com>
2018-02-14 22:58:45 +0100

    initial commit, setting up env's and master/slave system

---
+++ index.js
@@ -0,0 +1,16 @@
+const CAMERA_PLACEMENT = process.env.CAMERA_PLACEMENT
+const ROLE = process.env.ROLE
+
+console.log("ROLE")
+if(!process.env.ROLE){
+    console.log("Please specify ROLE as either SLAVE or MASTER in env-vars")
+    process.exit()
+}
+
+if(!process.env.CAMERA_PLACEMENT){
+    console.log("Please specify CAMERA_PLACEMENT as either 1, 2, 3, 4 in env-vars")
+    process.exit()
+}
+
+console.log(CAMERA_PLACEMENT)
+
