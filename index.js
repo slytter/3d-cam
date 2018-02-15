b29d521a40fe876ebccda27ceaff2214f98a4715
nikolaj schlüter.nielsen <nikolaj.sn@hotmail.com>
2018-02-15 01:16:01 +0100

    implemented part requests

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const slaveRouter = require('./lib/slaveRouter');
+const master = require('./lib/master');
 
 const CAMERA_PLACEMENT = process.env.CAMERA_PLACEMENT
 const ROLE = process.env.ROLE
-console.log("ROLE")
 if(!process.env.ROLE){
     console.log("Please specify ROLE as either SLAVE or MASTER in env-vars")
     process.exit()
@@ -13,6 +13,8 @@ if(!process.env.CAMERA_PLACEMENT){
     process.exit()
 }
 
-console.log(CAMERA_PLACEMENT)
-
-slaveRouter();
+if(ROLE == "SLAVE"){
+    slaveRouter();
+}else{
+    master();
+}
